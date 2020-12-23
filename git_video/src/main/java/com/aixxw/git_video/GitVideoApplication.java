9079c031ca64027609dc362f3214fad678aab761
AIxxw <1219129554@qq.com>
2020-12-23 10:35:36 +0800

    Initial commit

---
+++ git_video/src/main/java/com/aixxw/git_video/GitVideoApplication.java
@@ -0,0 +1,13 @@
+package com.aixxw.git_video;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitVideoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitVideoApplication.class, args);
+	}
+
+}
---
+++ git_video/src/test/java/com/aixxw/git_video/GitVideoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aixxw.git_video;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitVideoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
